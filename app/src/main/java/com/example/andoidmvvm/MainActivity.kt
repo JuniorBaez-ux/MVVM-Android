39599eea0e649c33302bd4c237e72a8665e506b6
Junior Baez <junior_baez@ucne.edu.do>
2022-05-03 10:06:53 -0400

    Implementando la funcionalidad princiapl del programa

--- app/src/main/java/com/example/andoidmvvm/MainActivity.kt
+++ app/src/main/java/com/example/andoidmvvm/MainActivity.kt
@@ -9,15 +9,30 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import com.example.andoidmvvm.databinding.ActivityMainBinding
+import viewmodel.QuoteViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val quoteViewModel: QuoteViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        quoteViewModel.quoteModel.observe(this, Observer {
+            binding.tvQuote.text = it.quote
+            binding.tvAuthor.text = it.author
+        })
+
+
+        binding.viewContainer.setOnClickListener { quoteViewModel.randomQuote() }
+
     }
+
 }
