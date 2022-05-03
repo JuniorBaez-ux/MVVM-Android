f2148002be2fa83dd5483009edd002d7d6e3fb4a
Junior Baez <junior_baez@ucne.edu.do>
2022-05-03 11:43:11 -0400

    Modificando generacion del QuoteModel

--- app/src/main/java/model/QuoteModel.kt
+++ app/src/main/java/model/QuoteModel.kt
@@ -1,3 +1,8 @@
 package model
 
-data class QuoteModel (val quote:String, val author:String)
+import com.google.gson.annotations.SerializedName
+
+data class QuoteModel(
+    @SerializedName("quote") val quote: String,
+    @SerializedName("author") val author: String
+)
