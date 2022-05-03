a00692609ff09eee2dc66d511b3f4ca5d4115863
Junior Baez <junior_baez@ucne.edu.do>
2022-05-03 09:52:03 -0400

    Preparando el ViewModel de la app

---
+++ app/src/main/java/viewmodel/QuoteViewModel.kt
@@ -0,0 +1,16 @@
+package viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import model.QuoteModel
+import model.QuoteProvider
+
+class QuoteViewModel : ViewModel() {
+
+    val quoteModel = MutableLiveData<QuoteModel>()
+
+    fun randomQuote() {
+        val currentQuote = QuoteProvider.random()
+        quoteModel.postValue(currentQuote)
+    }
+}
