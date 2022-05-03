b2a1b8cf0bc16e96316821c54e7cf4b230c073dc
Junior Baez <junior_baez@ucne.edu.do>
2022-05-03 12:36:16 -0400

    Implementando cambios finales del clean arquitecture

---
+++ app/src/main/java/domain/GetRandomQuoteUseCase.kt
@@ -0,0 +1,16 @@
+package domain
+
+import model.QuoteModel
+import model.QuoteProvider
+
+class GetRandomQuoteUseCase {
+
+    operator fun invoke(): QuoteModel?{
+        val quotes = QuoteProvider.quotes
+        if(!quotes.isNullOrEmpty()){
+            val randomNumber = (quotes.indices).random()
+            return quotes[randomNumber]
+        }
+        return null
+    }
+}
--- app/src/main/java/ui/viewmodel/QuoteViewModel.kt
+++ app/src/main/java/ui/viewmodel/QuoteViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import domain.GetQuotesUseCase
+import domain.GetRandomQuoteUseCase
 import kotlinx.coroutines.launch
 import model.QuoteModel
 import model.QuoteProvider
@@ -13,7 +14,16 @@ class QuoteViewModel : ViewModel() {
     val quoteModel = MutableLiveData<QuoteModel>()
 
     fun randomQuote() {
+        var getRandomQuoteUseCase = GetRandomQuoteUseCase()
 
+        fun randomQuote() {
+            isLoading.postValue(true)
+            val quote = getRandomQuoteUseCase()
+            if(quote!=null){
+                quoteModel.postValue(quote!!)
+            }
+            isLoading.postValue(false)
+        }
     }
     val isLoading = MutableLiveData<Boolean>()
     var getQuotesUseCase = GetQuotesUseCase()
