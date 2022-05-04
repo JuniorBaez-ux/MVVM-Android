e3cfa96f853c58eb7ef82568b44f8f3c8ef180f8
Junior Baez <junior_baez@ucne.edu.do>
2022-05-03 22:31:56 -0400

    Implementando la funcionalidad de obtener entidades desde la base de datos

--- app/src/main/java/domain/GetQuotesUseCase.kt
+++ app/src/main/java/domain/GetQuotesUseCase.kt
@@ -1,9 +1,22 @@
 package domain
 
 import data.QuoteRepository
+import data.database.entities.toDatabase
+import domain.model.Quote
 import model.QuoteModel
 import javax.inject.Inject
 
 class GetQuotesUseCase @Inject constructor(private val repository: QuoteRepository) {
-    suspend operator fun invoke() = repository.getAllQuotes()
+    suspend operator fun invoke():List<Quote> {
+        val quotes = repository.getAllQuotesFromApi()
+
+        return if (quotes.isNotEmpty()){
+
+            repository.insertQuotes(quotes.map {it.toDatabase()})
+            quotes
+        }
+        else{
+            repository.getAllQuotesFromDatabase()
+        }
+    }
 }
